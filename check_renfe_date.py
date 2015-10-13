8686dda14d8797d87ca6f9c6506dca1d060c9884
Javi Corbin <javi.corbin@gmail.com>
2015-10-13 16:24:56 +0200

    Adding mandrill as email sender.

--- check_renfe_date.py
+++ check_renfe_date.py
@@ -1,10 +1,10 @@
-#!/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # State: alpha
 
 from bs4 import BeautifulSoup as bs
-import json
 import ConfigParser
+import mandrill
 import re
 import requests
 
@@ -17,7 +17,8 @@ def check_renfe_date():
     origin = Config.get('renfe', 'origin')
     dest = Config.get('renfe', 'dest')
     date = Config.get('renfe', 'date').replace('-', '%2F')
-
+    mandrill_key = Config.get('renfe', 'mandrill_key')
+    
     estaciones_payload = "callCount=1&windowName=&c0-scriptName=estacionesManager&c0-methodName=getEstacionesIntAuto&c0-id=0&batchId=1&instanceId=0&page=%2Fvol%2Findex.do&scriptSessionId=o*ygDPpho6ORWkCO1ve8EDGao1l/3ocIo1l-VrlGaOSP6"
     payload = "cdgoOrigen=%s&cdgoDestino=%s&tipoBusqueda=autocomplete&desOrigen=%s&desDestino=%s&ninos=0&currenLocation=menuBusqueda&operation=&grupos=false&tipoOperacion=IND&empresas=false&getTarifasSesion=false&adultos=1&actTipoViajero=true&IdOrigen=%s&IdDestino=%s&FechaIdaSel=%s&HoraIdaSel=00%%3A00&FechaVueltaSel=&HoraVueltaSel=00%%3A00&adultos_=1&ninos_=0&ninosMenores=0&codPromocional=&txtoUsuario=&txtoPass="
 
@@ -34,8 +35,8 @@ def check_renfe_date():
     }
 
     session = requests.Session()
-    estaciones = session.post('https://venta.renfe.com/vol/dwr/call/plaincall/estacionesManager.getEstacionesIntAuto.dwr', data=estaciones_payload, headers=headers).text
-    tuplas = re.findall('c:"([0-9nul]+,[0-9nul]+,[0-9nul]+)"|,d:"([A-z \(\*\)\.\'/\-0-9]+)"', estaciones)
+    stations = session.post('https://venta.renfe.com/vol/dwr/call/plaincall/estacionesManager.getEstacionesIntAuto.dwr', data=estaciones_payload, headers=headers).text
+    tuplas = re.findall('c:"([0-9nul]+,[0-9nul]+,[0-9nul]+)"|,d:"([A-z \(\*\)\.\'/\-0-9]+)"', stations)
     station_codes = dict()
     for tupla in tuplas:
         if tupla[0].count('') > 1:
@@ -54,11 +55,12 @@ def check_renfe_date():
         print "Almost one station doesn't exists."
     response = session.post(url, data=payload, headers=headers).text
     if "Se ha producido un error" in response:
-        print response
+        # print response
         print "Error"
     elif "AVE" in response:
         res = bs(response, 'html.parser')
         tabla = '<meta charset="UTF-8">' + res.html.body.table
+        mandrill_client = mandrill.Mandrill(mandrill_key)
         print "Buy your ticket"
 
     #Send mail with the table
